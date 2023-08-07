9d1a5e8dc01a74669cfd06c1ac5a41018dc5b559
陆伟 <oao.lu@icloud.com>
2023-08-07 14:59:05 +0800

    Initial Commit

---
+++ color-picker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  color-picker
+//
+//  Created by oao on 2023/8/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ color-picker/color_pickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  color_pickerApp.swift
+//  color-picker
+//
+//  Created by oao on 2023/8/7.
+//
+
+import SwiftUI
+
+@main
+struct color_pickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
